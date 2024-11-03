8e488cd20dc71a47b722287ce2ae6cab73d317ab
ahmettahaklc <ahmettahaklc@gmail.com>
2024-11-03 23:31:27 +0300

    Create DFS and DFS(Limited) search algorithm

--- src/Main.java
+++ src/Main.java
@@ -128,6 +128,67 @@ public class Main {
         }
     }
 
+    public static void DFSLimited(int[][] a) {
+        int column = 0;
+        int line = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (a[i][j] == 0) {
+                    line = i;
+                    column = j;
+                }
+            }
+        }
+
+        if (line > 0) {
+            int[][] fringe = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    fringe[i][j] = a[i][j];
+                }
+            }
+            fringe[line][column] = fringe[line - 1][column];
+            fringe[line - 1][column] = 0;
+            queue.addFirst(fringe);
+        }
+
+        if (line < 2) {
+            int[][] fringe = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    fringe[i][j] = a[i][j];
+                }
+            }
+            fringe[line][column] = fringe[line + 1][column];
+            fringe[line + 1][column] = 0;
+            queue.addFirst(fringe);
+        }
+
+        if (column > 0) {
+            int[][] fringe = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    fringe[i][j] = a[i][j];
+                }
+            }
+            fringe[line][column] = fringe[line][column - 1];
+            fringe[line][column - 1] = 0;
+            queue.addFirst(fringe);
+        }
+
+        if (column < 2) {
+            int[][] fringe = new int[3][3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    fringe[i][j] = a[i][j];
+                }
+            }
+            fringe[line][column] = fringe[line][column + 1];
+            fringe[line][column + 1] = 0;
+            queue.addFirst(fringe);
+        }
+    }
+
     public static boolean CheckingState(int[][] currentState) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -144,7 +205,13 @@ public class Main {
         int[][] initial_state = {{3, 6, 1}, {7, 0, 2}, {4, 8, 5}};
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please choose search algorithm\n1-BFS");
+        System.out.println("""
+                Please choose search algorithm for find out the goal state
+                
+                1-BFS
+                2-DFS
+                3-DFS(Limited)""");
+
         String choose = scanner.nextLine();
 
         if (choose.equals("1")) {
@@ -158,7 +225,7 @@ public class Main {
                     }
                     System.out.println();
                 }
-                System.out.println("------");
+                System.out.println("---");
 
                 if (CheckingState(currentState)) {
                     System.out.println("Goal State is founded");
@@ -166,7 +233,49 @@ public class Main {
                 }
 
                 BFS(currentState);
-                
+
+            }
+        } else if (choose.equals("2")) {
+            queue.add(initial_state);
+            while (!queue.isEmpty()) {
+                int[][] currentState = queue.removeFirst();
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        System.out.print(currentState[i][j]);
+                    }
+                    System.out.println();
+                }
+                System.out.println("---");
+
+                if (CheckingState(currentState)) {
+                    System.out.println("Goal State is founded");
+                    break;
+                }
+
+                DFS(currentState);
+
+            }
+        }
+
+        else if (choose.equals("3")) {
+            queue.add(initial_state);
+            while (!queue.isEmpty()) {
+                int[][] currentState = queue.removeFirst();
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        System.out.print(currentState[i][j]);
+                    }
+                    System.out.println();
+                }
+                System.out.println("---");
+
+                if (CheckingState(currentState)) {
+                    System.out.println("Goal State is founded");
+                    break;
+                }
+
+                DFSLimited(currentState);
+
             }
         }
 
